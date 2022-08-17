d68819765e603315a899e39ee3717b9bcbbde4f4
HM TAhir <tahir4work@gmail.com>
2022-08-18 01:23:14 +0400

    requirements added once again

--- PlanDubai/settings.py
+++ PlanDubai/settings.py
@@ -113,6 +113,9 @@ DATABASES = {
 # }
 # import dj_database_url
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
 
 
 # Password validation
