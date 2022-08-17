08a9c08d8014eff9fb2d7c58a41e88d3462d0e17
HM TAhir <tahir4work@gmail.com>
2022-08-17 20:48:30 +0400

    first commit

--- PlanDubai/settings.py
+++ PlanDubai/settings.py
@@ -169,7 +169,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://plandubaivisit.com/",
 ]
 # CORS_ALLOW_ALL_ORIGINS: True
 
