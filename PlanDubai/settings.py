96e0c7216a00efb6de952531cb541f0e1b73e61d
HMTAhir327 <tahir4work@gmail.com>
2022-09-12 22:25:58 +0500

    sec instance of ec2

--- PlanDubai/settings.py
+++ PlanDubai/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ngc=m7mx=1so11+&w(mh84uz%$40h998ba&!+ok@y_fbhd3e_1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['plandubaivisit.herokuapp.com','127.0.0.1','18.208.60.216']
+ALLOWED_HOSTS = ['plandubaivisit.herokuapp.com','127.0.0.1','18.208.60.216','13.233.144.62','localhost']
 
 
 # Application definition
